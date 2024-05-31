d7dc2709382fcd8025fed127fa831d78c721c357
Metaldev <fernydiaz62@gmail.com>
2024-05-31 16:57:58 -0300

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import db from "@astrojs/db";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), db()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: "@astrojs/vercel",
 });
